21871e7985d72cf4d02acb8fc4287c96792c10a5
Glumist <glumist@gmail.com>
2018-01-18 17:08:21 +0300

    first commit

---
+++ ParserTreeView/MainForm.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace ParserTreeView
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.btParse = new System.Windows.Forms.Button();
+            this.tvStructure = new System.Windows.Forms.TreeView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tsTree = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tsbSearch = new System.Windows.Forms.ToolStripButton();
+            this.tstbSearchText = new System.Windows.Forms.ToolStripTextBox();
+            this.tbFullText = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.tsbParseText = new System.Windows.Forms.ToolStripButton();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tsTree.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Адрес";
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbAddress.Location = new System.Drawing.Point(13, 29);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(785, 20);
+            this.tbAddress.TabIndex = 1;
+            this.tbAddress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbAddress_KeyDown);
+            // 
+            // btParse
+            // 
+            this.btParse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btParse.Location = new System.Drawing.Point(804, 26);
+            this.btParse.Name = "btParse";
+            this.btParse.Size = new System.Drawing.Size(75, 23);
+            this.btParse.TabIndex = 2;
+            this.btParse.Text = "Распарсить";
+            this.btParse.UseVisualStyleBackColor = true;
+            this.btParse.Click += new System.EventHandler(this.btParse_Click);
+            // 
+            // tvStructure
+            // 
+            this.tvStructure.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvStructure.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tvStructure.Location = new System.Drawing.Point(0, 25);
+            this.tvStructure.Name = "tvStructure";
+            this.tvStructure.Size = new System.Drawing.Size(692, 563);
+            this.tvStructure.TabIndex = 3;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tvStructure);
+            this.splitContainer1.Panel1.Controls.Add(this.tsTree);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tbFullText);
+            this.splitContainer1.Panel2.Controls.Add(this.toolStrip1);
+            this.splitContainer1.Size = new System.Drawing.Size(891, 588);
+            this.splitContainer1.SplitterDistance = 692;
+            this.splitContainer1.SplitterWidth = 6;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // tsTree
+            // 
+            this.tsTree.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.tsTree.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.tsbSearch,
+            this.tstbSearchText});
+            this.tsTree.Location = new System.Drawing.Point(0, 0);
+            this.tsTree.Name = "tsTree";
+            this.tsTree.Size = new System.Drawing.Size(692, 25);
+            this.tsTree.TabIndex = 5;
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(63, 22);
+            this.toolStripLabel1.Text = "Структура";
+            // 
+            // tsbSearch
+            // 
+            this.tsbSearch.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.tsbSearch.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbSearch.Image = global::ParserTreeView.Properties.Resources.IconSearch;
+            this.tsbSearch.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSearch.Name = "tsbSearch";
+            this.tsbSearch.Size = new System.Drawing.Size(23, 22);
+            this.tsbSearch.Text = "Поиск";
+            this.tsbSearch.Click += new System.EventHandler(this.tsbSearch_Click);
+            // 
+            // tstbSearchText
+            // 
+            this.tstbSearchText.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.tstbSearchText.Name = "tstbSearchText";
+            this.tstbSearchText.Size = new System.Drawing.Size(100, 25);
+            this.tstbSearchText.TextChanged += new System.EventHandler(this.tstbSearchText_TextChanged);
+            // 
+            // tbFullText
+            // 
+            this.tbFullText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbFullText.Location = new System.Drawing.Point(0, 25);
+            this.tbFullText.Multiline = true;
+            this.tbFullText.Name = "tbFullText";
+            this.tbFullText.Size = new System.Drawing.Size(193, 563);
+            this.tbFullText.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel2,
+            this.tsbParseText});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(193, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(37, 22);
+            this.toolStripLabel2.Text = "Текст";
+            // 
+            // tsbParseText
+            // 
+            this.tsbParseText.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbParseText.Image = ((System.Drawing.Image)(resources.GetObject("tsbParseText.Image")));
+            this.tsbParseText.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbParseText.Name = "tsbParseText";
+            this.tsbParseText.Size = new System.Drawing.Size(74, 22);
+            this.tsbParseText.Text = "Распарсить";
+            this.tsbParseText.Click += new System.EventHandler(this.btParse_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer2.Location = new System.Drawing.Point(0, 55);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer2.Size = new System.Drawing.Size(891, 674);
+            this.splitContainer2.SplitterDistance = 543;
+            this.splitContainer2.SplitterWidth = 6;
+            this.splitContainer2.TabIndex = 6;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(103, 53);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 729);
+            this.Controls.Add(this.splitContainer2);
+            this.Controls.Add(this.btParse);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Парсер";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tsTree.ResumeLayout(false);
+            this.tsTree.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.Button btParse;
+        private System.Windows.Forms.TreeView tvStructure;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox tbFullText;
+        private System.Windows.Forms.ToolStrip tsTree;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox tstbSearchText;
+        private System.Windows.Forms.ToolStripButton tsbSearch;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripButton tsbParseText;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ ParserTreeView/MainForm.cs
@@ -0,0 +1,146 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParserTreeView
+{
+    public partial class MainForm : Form
+    {
+        List<TreeNode> _allNodes;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _allNodes = new List<TreeNode>();
+        }
+
+        private void btParse_Click(object sender, EventArgs e)
+        {
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+
+            if (sender == btParse)
+            {
+                if (string.IsNullOrWhiteSpace(tbAddress.Text))
+                    return;
+                string htmlString = GetHtmlString(tbAddress.Text);
+                doc.LoadHtml(htmlString);
+                //webBrowser1.Url = new Uri(tbAddress.Text);
+                webBrowser1.DocumentText = htmlString;
+            }
+            else if (sender == tsbParseText)
+                doc.LoadHtml(tbFullText.Text);
+            else
+                return;
+
+            tbFullText.Text = doc.DocumentNode.OuterHtml;
+            tvStructure.Nodes.Clear();
+            _allNodes.Clear();
+            TreeNode rootNode = GetNode(doc.DocumentNode, _allNodes);
+            tvStructure.Nodes.Add(rootNode);
+            _allNodes.Add(rootNode);
+        }
+
+        private void tbAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btParse_Click(sender, e);
+        }
+
+        public string GetHtmlString(string url)
+        {
+            WebClient client = new WebClient();
+            client.Proxy = null;
+            client.Encoding = System.Text.Encoding.GetEncoding("utf-8");
+            client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:18.0) Gecko/20100101 Firefox/18.0");
+            return client.DownloadString(url);
+        }
+
+        public static TreeNode GetNode(HtmlNode node, List<TreeNode> nodesList)
+        {
+            TreeNode treeNode = new TreeNode(node.Name + GetNodeAttributes(node));
+
+            if (node.HasChildNodes)
+            {
+                foreach (HtmlNode child in node.ChildNodes)
+                {
+                    TreeNode childNode = GetNode(child, nodesList);
+                    if (childNode != null)
+                    {
+                        treeNode.Nodes.Add(childNode);
+                        nodesList.Add(childNode);
+                    }
+                }
+            }
+            else if (node.Name != "#text" || !string.IsNullOrWhiteSpace(node.InnerText))
+                treeNode.Text += ":" + node.InnerText;
+            else
+                return null;
+
+            return treeNode;
+        }
+
+        private static string GetNodeAttributes(HtmlNode node)
+        {
+            string result = "(";
+            foreach (HtmlAttribute att in node.Attributes)
+                result += att.Name + "=" + att.Value + ";";
+            if (result.Length > 1)
+                result = result.Remove(result.Length - 1);
+            result += ")";
+            return result;
+        }
+
+        #region Search
+
+        private TreeNode[] _currentNodeMatches;
+        private int _nextNodeIndex = 0;
+
+        private void tstbSearchText_TextChanged(object sender, EventArgs e)
+        {
+            _currentNodeMatches = null;
+        }
+
+        private void tsbSearch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tstbSearchText.Text))
+                return;
+
+            if (_currentNodeMatches == null)
+            {
+                //It's a new Search
+                _nextNodeIndex = 0;
+                _currentNodeMatches = FindNodes(tstbSearchText.Text);
+            }
+
+            if (_nextNodeIndex >= 0 && _currentNodeMatches.Length > 0 && _nextNodeIndex < _currentNodeMatches.Length)
+            {
+                TreeNode selectedNode = _currentNodeMatches[_nextNodeIndex];
+                _nextNodeIndex++;
+                tvStructure.SelectedNode = selectedNode;
+                tvStructure.SelectedNode.Expand();
+                tvStructure.Select();
+            }
+        }
+
+        private TreeNode[] FindNodes(string searchText)
+        {
+            List<TreeNode> foundNodes = new List<TreeNode>();
+            foreach (TreeNode node in _allNodes)
+                if (node.Text.ToLower().Contains(searchText.ToLower()))
+                    foundNodes.Add(node);
+
+            return foundNodes.ToArray();
+        }
+
+        #endregion
+    }
+}
